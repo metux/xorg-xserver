594029dd9008cb7fbd42bf48f7a9f6217cdf333e
Olivier Fourdan <ofourdan@redhat.com>
2023-04-06 16:12:54 +0200

    glamor: Fix build without GBM
    The functions glamor_egl_fd_from_pixmap()/glamor_egl_fds_from_pixmap()
    are not available without GBM support.
    
    So if GBM is not available or too old, the code would fail to link
    trying to find the references to those functions.
    
    Make sure we skip that code when glamor is built without GBM.
    
    Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>
    (cherry picked from commit c24910d0e138dc1dc58c0c0cdc10b49a1ed85975)

--- glamor/glamor.c
+++ glamor/glamor.c
@@ -990,6 +990,7 @@ _glamor_fds_from_pixmap(ScreenPtr screen, PixmapPtr pixmap, int *fds,
                         uint32_t *strides, uint32_t *offsets,
                         CARD32 *size, uint64_t *modifier)
 {
+#ifdef GLAMOR_HAS_GBM
     glamor_pixmap_private *pixmap_priv = glamor_get_pixmap_private(pixmap);
     glamor_screen_private *glamor_priv =
         glamor_get_screen_private(pixmap->drawable.pScreen);
@@ -1017,6 +1018,7 @@ _glamor_fds_from_pixmap(ScreenPtr screen, PixmapPtr pixmap, int *fds,
     default:
         break;
     }
+#endif /* GLAMOR_HAS_GBM */
     return 0;
 }
 
