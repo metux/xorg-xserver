3db39041be97fc56ea224cf46dd07827a82b4973
Enrico Weigelt, metux IT consult <info@metux.net>
2024-01-30 15:15:26 +0100

    os: access: dop unused GetAccessControl()
    the function really doesn't seem to be used anymore, so it can be dropped.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- os/access.c
+++ os/access.c
@@ -1637,13 +1637,6 @@ ChangeAccessControl(ClientPtr client, int fEnabled)
     return Success;
 }
 
-/* returns FALSE if xhost + in effect, else TRUE */
-int
-GetAccessControl(void)
-{
-    return AccessEnabled;
-}
-
 int
 GetClientFd(ClientPtr client)
 {
