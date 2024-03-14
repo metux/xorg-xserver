a13fab3da1d5975826b659384de243c2af60b9d5
Enrico Weigelt, metux IT consult <info@metux.net>
2024-03-14 13:14:43 +0100

    (submit/xf86_os_privs) xfree86: os-support: move xf86OSInitVidMem() to xf86_os_support.h
    This function isn't exported at all, so it better fits into xf86_os_support.h,
    whose purpose is being the primary entry point into os-support layer.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/os-support/solaris/sun_vid.c
+++ hw/xfree86/os-support/solaris/sun_vid.c
@@ -49,13 +49,6 @@
 #endif
 
 #include <sys/types.h>          /* get __x86 definition if not set by compiler */
-
-#if defined(__i386__) || defined(__i386) || defined(__x86)
-#include <sys/tss.h>
-#include <sys/sysi86.h>
-#include <sys/psw.h>
-#endif /* defined(__i386__) || defined(__i386) || defined(__x86) */
-
 #include <sys/mman.h>
 
 #include "xf86.h"
