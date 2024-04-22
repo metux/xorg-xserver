00796cbf8cf8e2f613aaa8b2400b5c63cddd0e5e
Enrico Weigelt, metux IT consult <info@metux.net>
2024-04-22 12:02:04 +0200

    mi: drop obsolete miOverlaySetRootClip()
    Not used by any (known) drivers.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1503>

--- mi/mioverlay.h
+++ mi/mioverlay.h
@@ -9,6 +9,4 @@
 typedef void (*miOverlayTransFunc) (ScreenPtr, int, BoxPtr);
 typedef Bool (*miOverlayInOverlayFunc) (WindowPtr);
 
-extern _X_EXPORT void miOverlaySetRootClip(ScreenPtr, Bool);
-
 #endif                          /* __MIOVERLAY_H */
