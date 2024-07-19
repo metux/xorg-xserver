646831cda260140eb83f6f3a3f2ad6d69cc9f3a5
Enrico Weigelt, metux IT consult <info@metux.net>
2024-07-19 13:38:58 +0200

    include: dixfontstr.h: drop silent dependency on libxfont2
    This header includes libxfont2.h, but the dependency isn't stated anywhere,
    causing some drivers to FTBS (when libxont2.h is in non-standard location).
    
    Since this header doesn't seem to need including libxfont2.h at all, just
    stop including it, instead of adding yet another dependency to server SDK.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1610>

--- hw/xnest/Font.c
+++ hw/xnest/Font.c
@@ -16,12 +16,14 @@ is" without express or implied warranty.
 #include <xnest-config.h>
 #endif
 
+#include <stddef.h>
 #include <X11/X.h>
 #include <X11/Xatom.h>
 #include <X11/Xdefs.h>
 #include <X11/Xproto.h>
 #include <X11/fonts/font.h>
 #include <X11/fonts/fontstruct.h>
+#include <X11/fonts/libxfont2.h>
 
 #include "misc.h"
 #include "regionstr.h"
--- hw/xnest/Init.c
+++ hw/xnest/Init.c
@@ -16,10 +16,12 @@ is" without express or implied warranty.
 #include <xnest-config.h>
 #endif
 
+#include <stddef.h>
 #include <X11/X.h>
 #include <X11/Xdefs.h>
 #include <X11/Xproto.h>
 #include <X11/fonts/fontstruct.h>
+#include <X11/fonts/libxfont2.h>
 
 #include "dix/screenint_priv.h"
 #include "os/ddx_priv.h"
