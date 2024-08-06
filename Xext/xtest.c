69982abf7b4da1c6f9c080248b6c8617ffbf32db
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-06 15:20:47 +0200

    Xext: xtest: drop swapping request length fields
    The request struct's length fields aren't used anymore - we have the
    client->req_len field instead, which also is bigreq-compatible.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1639>

--- Xext/xtest.c
+++ Xext/xtest.c
@@ -373,7 +373,6 @@ ProcXTestFakeInput(ClientPtr client)
         /* swap the request back so we can simply re-execute it */
         if (client->swapped) {
             (void) XTestSwapFakeInput(client, (xReq *) stuff);
-            swaps(&stuff->length);
         }
         ResetCurrentRequest(client);
         client->sequence--;
@@ -487,8 +486,6 @@ static int _X_COLD
 SProcXTestGetVersion(ClientPtr client)
 {
     REQUEST(xXTestGetVersionReq);
-
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xXTestGetVersionReq);
     swaps(&stuff->minorVersion);
     return ProcXTestGetVersion(client);
@@ -498,8 +495,6 @@ static int _X_COLD
 SProcXTestCompareCursor(ClientPtr client)
 {
     REQUEST(xXTestCompareCursorReq);
-
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xXTestCompareCursorReq);
     swapl(&stuff->window);
     swapl(&stuff->cursor);
@@ -537,7 +532,6 @@ SProcXTestFakeInput(ClientPtr client)
 
     REQUEST(xReq);
 
-    swaps(&stuff->length);
     n = XTestSwapFakeInput(client, stuff);
     if (n != Success)
         return n;
@@ -547,10 +541,6 @@ SProcXTestFakeInput(ClientPtr client)
 static int _X_COLD
 SProcXTestGrabControl(ClientPtr client)
 {
-    REQUEST(xXTestGrabControlReq);
-
-    swaps(&stuff->length);
-    REQUEST_SIZE_MATCH(xXTestGrabControlReq);
     return ProcXTestGrabControl(client);
 }
 
