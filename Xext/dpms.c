fb40f65ee020b53c34e2b867cda662388f098ea3
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-06 14:47:45 +0200

    Xext: dpms: drop swapping request length fields
    The request struct's length fields aren't used anymore - we have the
    client->req_len field instead, which also is bigreq-compatible.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1639>

--- Xext/dpms.c
+++ Xext/dpms.c
@@ -493,8 +493,6 @@ static int _X_COLD
 SProcDPMSGetVersion(ClientPtr client)
 {
     REQUEST(xDPMSGetVersionReq);
-
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xDPMSGetVersionReq);
     swaps(&stuff->majorVersion);
     swaps(&stuff->minorVersion);
@@ -504,22 +502,12 @@ SProcDPMSGetVersion(ClientPtr client)
 static int _X_COLD
 SProcDPMSCapable(ClientPtr client)
 {
-    REQUEST(xDPMSCapableReq);
-
-    swaps(&stuff->length);
-    REQUEST_SIZE_MATCH(xDPMSCapableReq);
-
     return ProcDPMSCapable(client);
 }
 
 static int _X_COLD
 SProcDPMSGetTimeouts(ClientPtr client)
 {
-    REQUEST(xDPMSGetTimeoutsReq);
-
-    swaps(&stuff->length);
-    REQUEST_SIZE_MATCH(xDPMSGetTimeoutsReq);
-
     return ProcDPMSGetTimeouts(client);
 }
 
@@ -527,8 +515,6 @@ static int _X_COLD
 SProcDPMSSetTimeouts(ClientPtr client)
 {
     REQUEST(xDPMSSetTimeoutsReq);
-
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xDPMSSetTimeoutsReq);
 
     swaps(&stuff->standby);
@@ -540,22 +526,12 @@ SProcDPMSSetTimeouts(ClientPtr client)
 static int _X_COLD
 SProcDPMSEnable(ClientPtr client)
 {
-    REQUEST(xDPMSEnableReq);
-
-    swaps(&stuff->length);
-    REQUEST_SIZE_MATCH(xDPMSEnableReq);
-
     return ProcDPMSEnable(client);
 }
 
 static int _X_COLD
 SProcDPMSDisable(ClientPtr client)
 {
-    REQUEST(xDPMSDisableReq);
-
-    swaps(&stuff->length);
-    REQUEST_SIZE_MATCH(xDPMSDisableReq);
-
     return ProcDPMSDisable(client);
 }
 
@@ -563,8 +539,6 @@ static int _X_COLD
 SProcDPMSForceLevel(ClientPtr client)
 {
     REQUEST(xDPMSForceLevelReq);
-
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xDPMSForceLevelReq);
 
     swaps(&stuff->level);
@@ -575,11 +549,6 @@ SProcDPMSForceLevel(ClientPtr client)
 static int _X_COLD
 SProcDPMSInfo(ClientPtr client)
 {
-    REQUEST(xDPMSInfoReq);
-
-    swaps(&stuff->length);
-    REQUEST_SIZE_MATCH(xDPMSInfoReq);
-
     return ProcDPMSInfo(client);
 }
 
@@ -587,7 +556,6 @@ static int _X_COLD
 SProcDPMSSelectInput(ClientPtr client)
 {
     REQUEST(xDPMSSelectInputReq);
-    swaps(&stuff->length);
     REQUEST_SIZE_MATCH(xDPMSSelectInputReq);
     swapl(&stuff->eventMask);
     return ProcDPMSSelectInput(client);
