c8c42490b74acbe8f3a0007b747a185edc50fa47
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-07 13:53:02 +0200

    Xext: shm: drop now obsolete swap procs
    Several SProc's have become no-ops, just calling the actual Proc's,
    so we can get rid of them entirely.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1639>

--- Xext/shm.c
+++ Xext/shm.c
@@ -1387,12 +1387,6 @@ SShmCompletionEvent(xShmCompletionEvent * from, xShmCompletionEvent * to)
 }
 
 static int _X_COLD
-SProcShmQueryVersion(ClientPtr client)
-{
-    return ProcShmQueryVersion(client);
-}
-
-static int _X_COLD
 SProcShmAttach(ClientPtr client)
 {
     REQUEST(xShmAttachReq);
@@ -1489,7 +1483,7 @@ SProcShmDispatch(ClientPtr client)
     REQUEST(xReq);
 
     if (stuff->data == X_ShmQueryVersion)
-        return SProcShmQueryVersion(client);
+        return ProcShmQueryVersion(client);
 
     if (!client->local)
         return BadRequest;
