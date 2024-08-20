b27fd4039e1e9ea7d236b7a2e0dc514c506c99cf
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-20 08:36:52 +0200

    (!1654) Xnest: replace X(Un)InstallColormap() by xcb_(un)install_colormap()
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xnest/Color.c
+++ hw/xnest/Color.c
@@ -309,7 +309,7 @@ xnestDirectInstallColormaps(ScreenPtr pScreen)
         dixLookupResourceByType((void **) &pCmap, pCmapIDs[i], X11_RESTYPE_COLORMAP,
                                 serverClient, DixInstallAccess);
         if (pCmap)
-            XInstallColormap(xnestDisplay, xnestColormap(pCmap));
+            xcb_install_colormap(xnestUpstreamInfo.conn, xnestColormap(pCmap));
     }
 }
 
@@ -330,7 +330,7 @@ xnestDirectUninstallColormaps(ScreenPtr pScreen)
         dixLookupResourceByType((void **) &pCmap, pCmapIDs[i], X11_RESTYPE_COLORMAP,
                                 serverClient, DixUninstallAccess);
         if (pCmap)
-            XUninstallColormap(xnestDisplay, xnestColormap(pCmap));
+            xcb_uninstall_colormap(xnestUpstreamInfo.conn, xnestColormap(pCmap));
     }
 }
 
