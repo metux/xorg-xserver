b155017c8e320644ab262c5ca9e269eec91ba227
Enrico Weigelt, metux IT consult <info@metux.net>
2024-08-20 16:05:56 +0200

    (!1654) Xnest: replace XConnectionNumber() by xcb_get_file_descriptor()
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xnest/Init.c
+++ hw/xnest/Init.c
@@ -134,7 +134,10 @@ InitInput(int argc, char *argv[])
 
     mieqInit();
 
-    SetNotifyFd(XConnectionNumber(xnestDisplay), xnestNotifyConnection, X_NOTIFY_READ, NULL);
+    SetNotifyFd(xcb_get_file_descriptor(xnestUpstreamInfo.conn),
+                xnestNotifyConnection,
+                X_NOTIFY_READ,
+                NULL);
 
     RegisterBlockAndWakeupHandlers(xnestBlockHandler, xnestWakeupHandler, NULL);
 }
