metux/xorg-xserver

commit f26f17c66a714a226d83dd45bd760288eff3de32

treewide: mark pGC->ops->CopyArea() calls not using result as void
We alread have several of these calls, that aren't interested in result value,
explicitly casting to void. Fixing this up for the remaining ones.

This is helpful for the human reader as well as quality analysis tools.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1648>

Xext/shm.c
@@ -482,7 +482,7 @@ doShmPutImage(DrawablePtr dst, GCPtr pGC,
                                          PixmapBytePad(w, depth), data);
         if (!pPixmap)
             return;
-        pGC->ops->CopyArea((DrawablePtr) pPixmap, dst, pGC, sx, sy, sw, sh, dx,
+        (void) pGC->ops->CopyArea((DrawablePtr) pPixmap, dst, pGC, sx, sy, sw, sh, dx,
                            dy);
         FreeScratchPixmapHeader(pPixmap);
     }

composite/compalloc.c
@@ -247,7 +247,7 @@ compRestoreWindow(WindowPtr pWin, PixmapPtr pPixmap)
             val.val = IncludeInferiors;
             ChangeGC(NullClient, pGC, GCSubwindowMode, &val);
             ValidateGC(&pWin->drawable, pGC);
-            (*pGC->ops->CopyArea) (&pPixmap->drawable,
+            (void) (*pGC->ops->CopyArea) (&pPixmap->drawable,
                                    &pWin->drawable, pGC, x, y, w, h, 0, 0);
             FreeScratchGC(pGC);
         }
@@ -552,7 +552,7 @@ compNewPixmap(WindowPtr pWin, int x, int y, int w, int h)
             val.val = IncludeInferiors;
             ChangeGC(NullClient, pGC, GCSubwindowMode, &val);
             ValidateGC(&pPixmap->drawable, pGC);
-            (*pGC->ops->CopyArea) (&pParent->drawable,
+            (void) (*pGC->ops->CopyArea) (&pParent->drawable,
                                    &pPixmap->drawable,
                                    pGC,
                                    x - pParent->drawable.x,

dbe/midbe.c
@@ -275,7 +275,7 @@ miDbeSwapBuffers(ClientPtr client, int *pNumWindows, DbeSwapInfoPtr swapInfo)
 
     case XdbeUntouched:
         ValidateGC((DrawablePtr) pDbeWindowPriv->pFrontBuffer, pGC);
-        (*pGC->ops->CopyArea) ((DrawablePtr) pWin,
+        (void) (*pGC->ops->CopyArea) ((DrawablePtr) pWin,
                                (DrawablePtr) pDbeWindowPriv->pFrontBuffer,
                                pGC, 0, 0, pWin->drawable.width,
                                pWin->drawable.height, 0, 0);
@@ -293,7 +293,7 @@ miDbeSwapBuffers(ClientPtr client, int *pNumWindows, DbeSwapInfoPtr swapInfo)
      */
 
     ValidateGC((DrawablePtr) pWin, pGC);
-    (*pGC->ops->CopyArea) ((DrawablePtr) pDbeWindowPriv->pBackBuffer,
+    (void) (*pGC->ops->CopyArea) ((DrawablePtr) pDbeWindowPriv->pBackBuffer,
                            (DrawablePtr) pWin, pGC, 0, 0,
                            pWin->drawable.width, pWin->drawable.height, 0, 0);
 
@@ -620,7 +620,7 @@ miDbePositionWindow(WindowPtr pWin, int x, int y)
         }
         /* Copy the contents of the old front pixmap to the new one. */
         if (pWin->bitGravity != ForgetGravity) {
-            (*pGC->ops->CopyArea) ((DrawablePtr) pDbeWindowPriv->pFrontBuffer,
+            (void) (*pGC->ops->CopyArea) ((DrawablePtr) pDbeWindowPriv->pFrontBuffer,
 				   (DrawablePtr) pFrontBuffer, pGC,
 				   sourcex, sourcey, savewidth, saveheight,
                                    destx, desty);
@@ -633,7 +633,7 @@ miDbePositionWindow(WindowPtr pWin, int x, int y)
         }
         /* Copy the contents of the old back pixmap to the new one. */
         if (pWin->bitGravity != ForgetGravity) {
-            (*pGC->ops->CopyArea) ((DrawablePtr) pDbeWindowPriv->pBackBuffer,
+            (void) (*pGC->ops->CopyArea) ((DrawablePtr) pDbeWindowPriv->pBackBuffer,
 				   (DrawablePtr) pBackBuffer, pGC,
                                    sourcex, sourcey, savewidth, saveheight,
                                    destx, desty);

dix/pixmap.c
@@ -278,7 +278,7 @@ PixmapDirtyCopyArea(PixmapPtr dst, DrawablePtr src,
         w = dst_box.x2 - dst_box.x1;
         h = dst_box.y2 - dst_box.y1;
 
-        pGC->ops->CopyArea(src, &dst->drawable, pGC,
+        (void) pGC->ops->CopyArea(src, &dst->drawable, pGC,
                            x + dst_box.x1, y + dst_box.y1, w, h,
                            dst_x + dst_box.x1, dst_y + dst_box.y1);
         b++;

glamor/glamor_egl.c
@@ -354,7 +354,7 @@ glamor_make_pixmap_exportable(PixmapPtr pixmap, Bool modifiers_ok)
 
     scratch_gc = GetScratchGC(pixmap->drawable.depth, screen);
     ValidateGC(&pixmap->drawable, scratch_gc);
-    scratch_gc->ops->CopyArea(&pixmap->drawable, &exported->drawable,
+    (void) scratch_gc->ops->CopyArea(&pixmap->drawable, &exported->drawable,
                               scratch_gc,
                               0, 0, width, height, 0, 0);
     FreeScratchGC(scratch_gc);

hw/xfree86/drivers/modesetting/dri2.c
@@ -320,7 +320,7 @@ ms_dri2_copy_region2(ScreenPtr screen, DrawablePtr drawable, RegionPtr pRegion,
      * callback chain so we know that will happen before the client
      * tries to render again.
      */
-    gc->ops->CopyArea(src, dst, gc,
+    (void) gc->ops->CopyArea(src, dst, gc,
                       0, 0,
                       drawable->width, drawable->height,
                       off_x, off_y);

hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -1565,7 +1565,7 @@ drmmode_copy_fb(ScrnInfoPtr pScrn, drmmode_ptr drmmode)
     gc = GetScratchGC(pScrn->depth, pScreen);
     ValidateGC(&dst->drawable, gc);
 
-    (*gc->ops->CopyArea)(&src->drawable, &dst->drawable, gc, 0, 0,
+    (void) (*gc->ops->CopyArea)(&src->drawable, &dst->drawable, gc, 0, 0,
                          pScrn->virtualX, pScrn->virtualY, 0, 0);
 
     FreeScratchGC(gc);

mi/mibitblt.c
@@ -465,7 +465,7 @@ miOpqStipDrawable(DrawablePtr pDraw, GCPtr pGC, RegionPtr prgnSrc,
     gcv[0].val = GXinvert;
     ChangeGC(NullClient, pGCT, GCFunction, gcv);
     ValidateGC((DrawablePtr) pPixmap, pGCT);
-    (*pGCT->ops->CopyArea) ((DrawablePtr) pPixmap, (DrawablePtr) pPixmap,
+    (void) (*pGCT->ops->CopyArea) ((DrawablePtr) pPixmap, (DrawablePtr) pPixmap,
                             pGCT, 0, 0, w + srcx, h, 0, 0);
 
     /* Swap foreground and background colors on the GC for the drawable.

commit c4481fc20fd4da1e89aa4f41a7dca1d563d00cee

treewide: fix indentions got broke by recent commit
Commit f26f17c66a714a226d83dd45bd760288eff3de32 broke some indentions,
fixing them now.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1662>

Xext/shm.c
@@ -482,8 +482,8 @@ doShmPutImage(DrawablePtr dst, GCPtr pGC,
                                          PixmapBytePad(w, depth), data);
         if (!pPixmap)
             return;
-        (void) pGC->ops->CopyArea((DrawablePtr) pPixmap, dst, pGC, sx, sy, sw, sh, dx,
-                           dy);
+        (void) pGC->ops->CopyArea((DrawablePtr) pPixmap, dst, pGC,
+                                  sx, sy, sw, sh, dx, dy);
         FreeScratchPixmapHeader(pPixmap);
     }
     else {

composite/compalloc.c
@@ -553,10 +553,11 @@ compNewPixmap(WindowPtr pWin, int x, int y, int w, int h)
             ChangeGC(NullClient, pGC, GCSubwindowMode, &val);
             ValidateGC(&pPixmap->drawable, pGC);
             (void) (*pGC->ops->CopyArea) (&pParent->drawable,
-                                   &pPixmap->drawable,
-                                   pGC,
-                                   x - pParent->drawable.x,
-                                   y - pParent->drawable.y, w, h, 0, 0);
+                                          &pPixmap->drawable,
+                                          pGC,
+                                          x - pParent->drawable.x,
+                                          y - pParent->drawable.y,
+                                          w, h, 0, 0);
             FreeScratchGC(pGC);
         }
     }

dbe/midbe.c
@@ -276,9 +276,9 @@ miDbeSwapBuffers(ClientPtr client, int *pNumWindows, DbeSwapInfoPtr swapInfo)
     case XdbeUntouched:
         ValidateGC((DrawablePtr) pDbeWindowPriv->pFrontBuffer, pGC);
         (void) (*pGC->ops->CopyArea) ((DrawablePtr) pWin,
-                               (DrawablePtr) pDbeWindowPriv->pFrontBuffer,
-                               pGC, 0, 0, pWin->drawable.width,
-                               pWin->drawable.height, 0, 0);
+                                      (DrawablePtr) pDbeWindowPriv->pFrontBuffer,
+                                      pGC, 0, 0, pWin->drawable.width,
+                                      pWin->drawable.height, 0, 0);
         break;
 
     case XdbeCopied:
@@ -294,8 +294,9 @@ miDbeSwapBuffers(ClientPtr client, int *pNumWindows, DbeSwapInfoPtr swapInfo)
 
     ValidateGC((DrawablePtr) pWin, pGC);
     (void) (*pGC->ops->CopyArea) ((DrawablePtr) pDbeWindowPriv->pBackBuffer,
-                           (DrawablePtr) pWin, pGC, 0, 0,
-                           pWin->drawable.width, pWin->drawable.height, 0, 0);
+                                  (DrawablePtr) pWin, pGC, 0, 0,
+                                  pWin->drawable.width,
+                                  pWin->drawable.height, 0, 0);
 
     /*
      **********************************************************************
@@ -621,9 +622,9 @@ miDbePositionWindow(WindowPtr pWin, int x, int y)
         /* Copy the contents of the old front pixmap to the new one. */
         if (pWin->bitGravity != ForgetGravity) {
             (void) (*pGC->ops->CopyArea) ((DrawablePtr) pDbeWindowPriv->pFrontBuffer,
-				   (DrawablePtr) pFrontBuffer, pGC,
-				   sourcex, sourcey, savewidth, saveheight,
-                                   destx, desty);
+                                          (DrawablePtr) pFrontBuffer, pGC,
+                                          sourcex, sourcey, savewidth, saveheight,
+                                          destx, desty);
         }
 
         ValidateGC(&pBackBuffer->drawable, pGC);
@@ -634,9 +635,9 @@ miDbePositionWindow(WindowPtr pWin, int x, int y)
         /* Copy the contents of the old back pixmap to the new one. */
         if (pWin->bitGravity != ForgetGravity) {
             (void) (*pGC->ops->CopyArea) ((DrawablePtr) pDbeWindowPriv->pBackBuffer,
-				   (DrawablePtr) pBackBuffer, pGC,
-                                   sourcex, sourcey, savewidth, saveheight,
-                                   destx, desty);
+                                          (DrawablePtr) pBackBuffer, pGC,
+                                          sourcex, sourcey, savewidth, saveheight,
+                                          destx, desty);
         }
 
         /* Destroy the old pixmaps, and point the DBE window priv to the new

dix/pixmap.c
@@ -278,9 +278,15 @@ PixmapDirtyCopyArea(PixmapPtr dst, DrawablePtr src,
         w = dst_box.x2 - dst_box.x1;
         h = dst_box.y2 - dst_box.y1;
 
-        (void) pGC->ops->CopyArea(src, &dst->drawable, pGC,
-                           x + dst_box.x1, y + dst_box.y1, w, h,
-                           dst_x + dst_box.x1, dst_y + dst_box.y1);
+        (void) pGC->ops->CopyArea(src,
+                                  &dst->drawable,
+                                  pGC,
+                                  x + dst_box.x1,
+                                  y + dst_box.y1,
+                                  w,
+                                  h,
+                                  dst_x + dst_box.x1,
+                                  dst_y + dst_box.y1);
         b++;
     }
     FreeScratchGC(pGC);