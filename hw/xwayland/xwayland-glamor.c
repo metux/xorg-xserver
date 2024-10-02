letoram/xarcan

commit 8e8bc0a1ef33a9e74a475a8f6e6869b53eec87a3

xwayland: Try manual redirection for surface window in glamor_check_flip
If the surface window already uses automatic redirection, we can upgrade
to manual redirection and save some blits with common use cases.

This fixes a minor performance regression from a65bb8480af8 ('Revert
"xwayland/glamor: Avoid implicit redirection with depth 32 parent
windows"') with mutter >= 44.

Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1570>

hw/xwayland/xwayland-glamor.c
@@ -93,6 +93,8 @@ xwl_glamor_check_flip(WindowPtr present_window, PixmapPtr pixmap)
 {
     ScreenPtr screen = pixmap->drawable.pScreen;
     PixmapPtr backing_pixmap = screen->GetWindowPixmap(present_window);
+    struct xwl_window *xwl_window = xwl_window_from_window(present_window);
+    WindowPtr surface_window = xwl_window->surface_window;
 
     if (pixmap->drawable.depth != backing_pixmap->drawable.depth) {
         if (pixmap->drawable.depth == 32)
@@ -100,6 +102,10 @@ xwl_glamor_check_flip(WindowPtr present_window, PixmapPtr pixmap)
 
         return xwl_present_maybe_redirect_window(present_window);
     }
+
+    if (surface_window->redirectDraw == RedirectDrawAutomatic &&
+        surface_window->drawable.depth != 32)
+        xwl_present_maybe_redirect_window(surface_window);
 
     return TRUE;
 }

metux/xorg-xserver

commit e6c649e6e6b7184c61500ed2740edb4ac7274a31

xwayland/glamor: Try manual redirect only if parent window has depth 32
That's the case 8e8bc0a1ef33 ("xwayland: Try manual redirection for
surface window in glamor_check_flip") was intended for. If the parent
window doesn't have depth 32, the automatic redirection might have been
done by a client, in which case doing the manual redirection may run
into trouble.

Fixes: 8e8bc0a1ef33 ("xwayland: Try manual redirection for surface window in glamor_check_flip")
Closes: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1724
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1637>

hw/xwayland/xwayland-glamor.c
@@ -104,7 +104,8 @@ xwl_glamor_check_flip(WindowPtr present_window, PixmapPtr pixmap)
     }
 
     if (surface_window->redirectDraw == RedirectDrawAutomatic &&
-        surface_window->drawable.depth != 32)
+        surface_window->drawable.depth != 32 &&
+        surface_window->parent->drawable.depth == 32)
         xwl_present_maybe_redirect_window(surface_window);
 
     return TRUE;