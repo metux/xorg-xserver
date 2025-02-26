41a75f01cc5b02c015d10699f3666c06fab3accd
Enrico Weigelt, metux IT consult <info@metux.net>
2025-02-26 17:57:26 +0100

    Revert "mi: drop obsolete miOverlayGetPrivateClips()"
    This reverts commit 2c21824ee96daf3db8025c8bbe1210050dd67a13.
    
    Requested by Nvidia - their proprietary driver still needs it.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1843>

--- mi/mioverlay.c
+++ mi/mioverlay.c
@@ -103,6 +103,24 @@ miOverlaySetRootClip(ScreenPtr pScreen, Bool enable)
 
 /****************************************************************/
 
+/* not used */
+Bool
+miOverlayGetPrivateClips(WindowPtr pWin,
+                         RegionPtr *borderClip, RegionPtr *clipList)
+{
+    miOverlayTreePtr pTree = MIOVERLAY_GET_WINDOW_TREE(pWin);
+
+    if (pTree) {
+        *borderClip = &(pTree->borderClip);
+        *clipList = &(pTree->clipList);
+        return TRUE;
+    }
+
+    *borderClip = *clipList = NULL;
+
+    return FALSE;
+}
+
 Bool
 miOverlayCopyUnderlay(ScreenPtr pScreen)
 {
