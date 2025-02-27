metux/xorg-xserver

commit c6f1b8a735d3c6ba80ca552b79e2dbe8a358ff40

randr: ProcRRGetCrtcTransform(): split reply header and payload
Using struct initializer for the reply header and only allocating the
payload on heap. This allows using generic macros for reply preparation
and send-out later.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1794>

randr/rrcrtc.c
@@ -1752,8 +1752,6 @@ ProcRRSetCrtcTransform(ClientPtr client)
                               filter, nbytes, params, nparams);
 }
 
-#define CrtcTransformExtra	(SIZEOF(xRRGetCrtcTransformReply) - 32)
-
 static int
 transform_filter_length(RRTransformPtr transform)
 {
@@ -1808,11 +1806,9 @@ int
 ProcRRGetCrtcTransform(ClientPtr client)
 {
     REQUEST(xRRGetCrtcTransformReq);
-    xRRGetCrtcTransformReply *reply;
     RRCrtcPtr crtc;
     int nextra;
     RRTransformPtr current, pending;
-    char *extra;
 
     REQUEST_SIZE_MATCH(xRRGetCrtcTransformReq);
     VERIFY_RR_CRTC(stuff->crtc, crtc, DixReadAccess);
@@ -1823,33 +1819,36 @@ ProcRRGetCrtcTransform(ClientPtr client)
     nextra = (transform_filter_length(pending) +
               transform_filter_length(current));
 
-    reply = calloc(1, sizeof(xRRGetCrtcTransformReply) + nextra);
-    if (!reply)
+    char *extra_buf = calloc(1, nextra);
+    if (!extra_buf)
         return BadAlloc;
 
-    extra = (char *) (reply + 1);
-    reply->type = X_Reply;
-    reply->sequenceNumber = client->sequence;
-    reply->length = bytes_to_int32(CrtcTransformExtra + nextra);
+    char *extra = extra_buf;
 
-    reply->hasTransforms = crtc->transforms;
+    xRRGetCrtcTransformReply rep = {
+        .type = X_Reply,
+        .sequenceNumber = client->sequence,
+        .length = bytes_to_int32(sizeof(xRRGetCrtcTransformReply) - sizeof(xReq)),
+        .hasTransforms = crtc->transforms,
+    };
 
-    transform_encode(client, &reply->pendingTransform, &pending->transform);
+    transform_encode(client, &rep.pendingTransform, &pending->transform);
     extra += transform_filter_encode(client, extra,
-                                     &reply->pendingNbytesFilter,
-                                     &reply->pendingNparamsFilter, pending);
+                                     &rep.pendingNbytesFilter,
+                                     &rep.pendingNparamsFilter, pending);
 
-    transform_encode(client, &reply->currentTransform, &current->transform);
+    transform_encode(client, &rep.currentTransform, &current->transform);
     extra += transform_filter_encode(client, extra,
-                                     &reply->currentNbytesFilter,
-                                     &reply->currentNparamsFilter, current);
+                                     &rep.currentNbytesFilter,
+                                     &rep.currentNparamsFilter, current);
 
     if (client->swapped) {
-        swaps(&reply->sequenceNumber);
-        swapl(&reply->length);
+        swaps(&rep.sequenceNumber);
+        swapl(&rep.length);
     }
-    WriteToClient(client, sizeof(xRRGetCrtcTransformReply) + nextra, reply);
-    free(reply);
+    WriteToClient(client, sizeof(xRRGetCrtcTransformReply), &rep);
+    WriteToClient(client, nextra, extra_buf);
+    free(extra_buf);
     return Success;
 }
 

commit 53876f1ef10eb9d513b126feda25e9fba5b375df

randr: fix RRGetCrtcTransform reply length, part 2
Mixed up reply vs request. Obviously, the size substracted from reply
struct's one has to be the one of the generic reply, not generic requst :o

Background: the meaning of the length field isn't entirely intuitive.

a) the size is in 4-byte units, instead of bytes (therefore passing through
   bytes_to_int32() call)
b) it's not the total packet size, but only the *extra* payload size, ergo:
   how much is the packet longer than a xGenericReply = 8 units = 32 bytes.
   (min. packet size is 32 bytes -> length = 0)

In order to prevent those kind of coding errors ever happening again, it might
be a good idea putting that into a generic macro.

Fixes: c6f1b8a735d3c6ba80ca552b79e2dbe8a358ff40
Fixes: 0ca5aaba500a4791b91f8237cbd74709331f8676
Issue: https://gitlab.freedesktop.org/xorg/xserver/-/issues/1797
Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>
Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1845>

randr/rrcrtc.c
@@ -1831,7 +1831,7 @@ ProcRRGetCrtcTransform(ClientPtr client)
     xRRGetCrtcTransformReply rep = {
         .type = X_Reply,
         .sequenceNumber = client->sequence,
-        .length = bytes_to_int32(sizeof(xRRGetCrtcTransformReply) - sizeof(xReq) + nextra),
+        .length = bytes_to_int32(sizeof(xRRGetCrtcTransformReply) - sizeof(xGenericReply) + nextra),
         .hasTransforms = crtc->transforms,
     };
 