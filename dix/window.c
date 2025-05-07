9b76094c5335d579ba5c93161b794d59d8d362c9
Enrico Weigelt, metux IT consult <info@metux.net>
2025-05-07 13:14:38 +0200

    (!1967) dix: extra NULL protection in UnmapSubwindows()
    Even though it probably won't be hit ever, it's still better to be
    really sure instead of some remote chance for hard segfault.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/window.c
+++ dix/window.c
@@ -2915,7 +2915,7 @@ UnmapSubwindows(WindowPtr pWin)
                 UnrealizeTree(pChild, FALSE);
         }
     }
-    if (wasViewable && anyMarked) {
+    if (wasViewable && anyMarked && pLayerWin) {
         if (pLayerWin->parent == pWin)
             (*pScreen->MarkWindow) (pWin);
         else {
