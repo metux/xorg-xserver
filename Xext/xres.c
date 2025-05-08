4bb65785c2274181388208c9e9c81208f4f3a226
Enrico Weigelt, metux IT consult <info@metux.net>
2025-05-08 16:55:00 +0200

    (!1970) Xext: xres: protect against free()ing from non-heap
    > ../Xext/xres.c: In function ‘DestroyFragments’:
    > ../Xext/xres.c:124:9: warning: ‘free’ of ‘it’ which points to memory on the stack [CWE-590] [-Wanalyzer-free-of-non-heap]
    >   124 |         free(it);
    >      |         ^~~~~~~~
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- Xext/xres.c
+++ Xext/xres.c
@@ -136,9 +136,11 @@ static void
 DestroyFragments(struct xorg_list *frags)
 {
     FragmentList *it, *tmp;
-    xorg_list_for_each_entry_safe(it, tmp, frags, l) {
-        xorg_list_del(&it->l);
-        free(it);
+    if (!xorg_list_is_empty(frags)) {
+        xorg_list_for_each_entry_safe(it, tmp, frags, l) {
+            xorg_list_del(&it->l);
+            free(it);
+        }
     }
 }
 
