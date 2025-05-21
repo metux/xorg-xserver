25b26d3a5dde6c81c70511426cdc99b39e1904ac
Enrico Weigelt, metux IT consult <info@metux.net>
2025-05-21 12:16:00 +0200

    xfree86: xf86Priv.h: unexport xf86LogVerbose field
    Not used by any external drivers.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/common/xf86_priv.h
+++ hw/xfree86/common/xf86_priv.h
@@ -43,6 +43,7 @@ extern const char *xf86LogFile;
 extern MessageType xf86LogFileFrom;
 extern Bool xf86LogFileWasOpened;
 extern int xf86Verbose;       /* verbosity level */
+extern int xf86LogVerbose;    /* log file verbosity level */
 
 extern int xf86NumDrivers;
 extern Bool xf86Resetting;
