db5ebdb62e0f563d3961cf95d424e4901550995b
Enrico Weigelt, metux IT consult <info@metux.net>
2025-05-21 17:11:31 +0200

    (!1992) xfree86: i2c: drop unused xf86I2CWriteBytes()
    Not used anywhere, so no need to keep around it any longer.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- hw/xfree86/i2c/xf86i2c.c
+++ hw/xfree86/i2c/xf86i2c.c
@@ -485,32 +485,6 @@ xf86I2CWriteByte(I2CDevPtr d, I2CByte subaddr, I2CByte byte)
     return xf86I2CWriteRead(d, wb, 2, NULL, 0);
 }
 
-/* Write bytes to subsequent registers determined by the
- * sub-address of the first register.
- */
-
-Bool
-xf86I2CWriteBytes(I2CDevPtr d, I2CByte subaddr,
-                  I2CByte * WriteBuffer, int nWrite)
-{
-    I2CBusPtr b = d->pI2CBus;
-    Bool r = TRUE;
-
-    if (nWrite > 0) {
-        r = b->I2CAddress(d, d->SlaveAddr & ~1);
-        if (r) {
-            if ((r = b->I2CPutByte(d, subaddr)))
-                for (; nWrite > 0; WriteBuffer++, nWrite--)
-                    if (!(r = b->I2CPutByte(d, *WriteBuffer)))
-                        break;
-
-            b->I2CStop(d);
-        }
-    }
-
-    return r;
-}
-
 /* Write a word (high byte, then low byte) to one of the registers
  * determined by its sub-address.
  */
