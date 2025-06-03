xseadgdc/xserver

(1867) dix: unexport and rename CreateWindow()
a) an internal function that's not used by any drivers
b) conflicting with function/define of same name on win32

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

dix/window_priv.h
@@ -1,0 +1,35 @@
+/* SPDX-License-Identifier: MIT OR X11
+ *
+ * Copyright © 2025 Enrico Weigelt, metux IT consult <info@metux.net>
+ */
+#ifndef _XSERVER_DIX_WINDOW_PRIV_H
+#define _XSERVER_DIX_WINDOW_PRIV_H
+
+#include <X11/X.h>
+
+#include "include/dix.h"
+#include "include/window.h"
+
+/*
+ * @brief create a window
+ *
+ * Creates a window with given XID, geometry, etc
+ *
+ * @return pointer to new Window or NULL on error (see error pointer)
+ */
+WindowPtr dixCreateWindow(Window wid,
+                          WindowPtr pParent,
+                          int x,
+                          int y,
+                          unsigned int w,
+                          unsigned int h,
+                          unsigned int bw,
+                          unsigned int windowclass,
+                          Mask vmask,
+                          XID * vlist,
+                          int depth,
+                          ClientPtr client,
+                          VisualID visual,
+                          int * error);
+
+#endif /* _XSERVER_DIX_WINDOW_PRIV_H */

metux/xorg-xserver

dix/window_priv.h
@@ -1,0 +1,35 @@
+/* SPDX-License-Identifier: MIT OR X11
+ *
+ * Copyright © 2025 Enrico Weigelt, metux IT consult <info@metux.net>
+ */
+#ifndef _XSERVER_DIX_WINDOW_H
+#define _XSERVER_DIX_WINDOW_H
+
+#include <X11/X.h>
+
+#include "include/dix.h"
+#include "include/window.h"
+
+/*
+ * @brief create a window
+ *
+ * Creates a window with given XID, geometry, etc
+ *
+ * @return pointer to new Window or NULL on error (see error pointer)
+ */
+WindowPtr dixCreateWindow(Window wid,
+                          WindowPtr pParent,
+                          int x,
+                          int y,
+                          unsigned int w,
+                          unsigned int h,
+                          unsigned int bw,
+                          unsigned int windowclass,
+                          Mask vmask,
+                          XID * vlist,
+                          int depth,
+                          ClientPtr client,
+                          VisualID visual,
+                          int * error);
+
+#endif /* _XSERVER_DIX_WINDOW_H */