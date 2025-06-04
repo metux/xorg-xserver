c4da97eb69c8f4e473ea58577ff7bd9d22f539e9
Enrico Weigelt, metux IT consult <info@metux.net>
2025-06-04 13:55:11 +0200

    Revert "(!1987) glamor: unexport GLAMOR_CREATE_PIXMAP_* defines"
    This reverts commit 85267d38ac5f50c62d61d233786bcd50c63bfb7a.
    
    These are needed by the RDP module (www.xrdp.org).
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- glamor/glamor_priv.h
+++ glamor/glamor_priv.h
@@ -175,13 +175,6 @@ enum gradient_shader {
     SHADER_GRADIENT_COUNT,
 };
 
-#define GLAMOR_CREATE_PIXMAP_CPU        0x100
-#define GLAMOR_CREATE_PIXMAP_FIXUP      0x101
-#define GLAMOR_CREATE_FBO_NO_FBO        0x103
-#define GLAMOR_CREATE_NO_LARGE          0x105
-#define GLAMOR_CREATE_PIXMAP_NO_TEXTURE 0x106
-#define GLAMOR_CREATE_FORMAT_CBCR       0x107
-
 struct glamor_screen_private;
 struct glamor_pixmap_private;
 
