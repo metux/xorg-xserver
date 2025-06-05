xseadgdc/xserver

Xnamespace: assign incoming clients to namespaces
The association is done by matching client's and namespace's
authentication token.

Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

miext/namespace/hooks.h
@@ -1,0 +1,28 @@
+#ifndef __XSERVER_NAMESPACE_HOOKS_H
+#define __XSERVER_NAMESPACE_HOOKS_H
+
+#include "dix/registry_priv.h"
+#include "include/misc.h"
+
+#include "namespace.h"
+
+#define XNS_HOOK_LOG(...) do { \
+        printf("Xns [" HOOK_NAME "] (#%d) {%s} <%s>: ", \
+            (client ? client->index : -1), \
+            (subj ? (subj->ns ? subj->ns->id : "(no ns)") : "<no client>"), \
+            LookupRequestName(client ? client->majorOp : 0, \
+                              client ? client->minorOp : 0)); \
+        printf(__VA_ARGS__); \
+    } while (0)
+
+#define XNS_HOOK_HEAD(t) \
+    t *param = calldata; \
+    ClientPtr client = param->client; \
+    if (!client) { \
+        /* XNS_LOG("hook %s NULL client\n", HOOK_NAME); */ \
+    } \
+    struct XnamespaceClientPriv *subj = XnsClientPriv(client);
+
+void hookClientState(CallbackListPtr *pcbl, void *unused, void *calldata);
+
+#endif /* __XSERVER_NAMESPACE_HOOKS_H */

metux/xorg-xserver

Xext/namespace/hooks.h
@@ -1,0 +1,29 @@
+#ifndef __XSERVER_NAMESPACE_HOOKS_H
+#define __XSERVER_NAMESPACE_HOOKS_H
+
+#include "dix/registry_priv.h"
+#include "include/misc.h"
+
+#include "namespace.h"
+
+#define XNS_HOOK_LOG(...) do { \
+        printf("XNS [" HOOK_NAME "] (#%d@%d) {%s} <%s>: ", \
+            (client ? client->index : -1), \
+            (client ? client->sequence : -1), \
+            (subj ? (subj->ns ? subj->ns->name : "(no ns)") : "<no client>"), \
+            LookupRequestName(client ? client->majorOp : 0, \
+                              client ? client->minorOp : 0)); \
+        printf(__VA_ARGS__); \
+    } while (0)
+
+#define XNS_HOOK_HEAD(t) \
+    t *param = calldata; \
+    ClientPtr client = param->client; \
+    if (!client) { \
+        /* XNS_LOG("hook %s NULL client\n", HOOK_NAME); */ \
+    } \
+    struct XnamespaceClientPriv *subj = XnsClientPriv(client);
+
+void hookClientState(CallbackListPtr *pcbl, void *unused, void *calldata);
+
+#endif /* __XSERVER_NAMESPACE_HOOKS_H */